cb4e3a084ac78d9869914e61e10b5b665565e5da
YaNoX1236 <andrew.aachen1906@gmail.com>
2024-03-22 19:16:50 +0100

    FMIT: add CORS;

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Weddings for DMF
 Start guide:
 1. Install requirements
 2. Start main file with unicorn option: --reload --env-file fmit.prod.env
-3. Open swagger: http://localhost:8000/docs#/
+3. Open swagger: `http://localhost:8000/docs#/`
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.configs.configs import configs
 from app.controllers.api import api_router
@@ -6,13 +7,19 @@ from app.db_connection import db_connection
 
 app = FastAPI()
 
-
 @app.on_event("startup")
 async def startup_event():
     await db_connection.init_db()
 
-app.include_router(api_router, prefix=configs.API_V1_STR)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
+app.include_router(api_router, prefix=configs.API_V1_STR)
 
 @app.get("/health")
 async def root():
